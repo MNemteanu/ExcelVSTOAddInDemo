52b7e4460c3f1015f68f5631530ae709498072e3
MNemteanu <mitchelfn@yahoo.com>
2020-02-10 16:47:39 -0700

    First commit

---
+++ VSTOExcelAddInDemo1/MyXMLRibbon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new MyXMLRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace VSTOExcelAddInDemo1
+{
+	[ComVisible(true)]
+	public class MyXMLRibbon : Office.IRibbonExtensibility
+	{
+		private Office.IRibbonUI ribbon;
+
+		public MyXMLRibbon()
+		{
+		}
+
+		#region IRibbonExtensibility Members
+
+		public string GetCustomUI(string ribbonID)
+		{
+			//return GetResourceText("VSTOExcelAddInDemo1.MyXMLRibbon.xml");
+			return VSTOExcelAddInDemo1.ThisAddIn.GetResourceText("VSTOExcelAddInDemo1.MyXMLRibbon.xml");
+		}
+
+		#endregion
+
+		#region Ribbon Callbacks
+		//Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+		public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+		{
+			this.ribbon = ribbonUI;
+		}
+
+		// Note for my callback methods:
+		// A method like this can be called from more than on control (if for some reason want to do that)
+		// and check the pControl.Id to decide what to do depending on what button was pressed.
+		// Notice that if the method signature does not match the expected signature then the method won't be called (no error will be thrown).
+
+		public void InstallVBA(Office.IRibbonControl pControl)
+		{
+			Globals.ThisAddIn.CheckVBACodeVersion();
+		}
+
+		public void TestVBACall_AddInMethod(Office.IRibbonControl pControl)
+		{
+			string workbookName = Globals.ThisAddIn.Application.ActiveWorkbook.Name;
+			Globals.ThisAddIn.Application.Workbooks[workbookName].Application.Run("TestVBACall_VBASub");
+		}
+
+		#endregion
+	}
+}
---
+++ VSTOExcelAddInDemo1/ThisAddIn.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+using System.IO;
+
+using VBA = Microsoft.Vbe.Interop;
+
+
+namespace VSTOExcelAddInDemo1
+{
+	public partial class ThisAddIn
+	{
+		private static string _latestVBACode;
+		private static int _latestVBACodeVersion;
+		private static string _vbaCodeFile = "VBATest.txt";
+		private static string _vbaCodeKey = "VBAVersion=";
+		Excel._Workbook _activeWorkbook;
+
+		private void ThisAddIn_Startup(object sender, System.EventArgs e)
+		{
+			this.Application.WorkbookActivate += Application_WorkbookActivate;
+		}
+
+		private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+		{
+		}
+
+		private void Application_WorkbookActivate(Excel.Workbook Wb)
+		{
+			// Trying to call from the XML Ribbon button instead
+			_activeWorkbook = this.Application.ActiveWorkbook;
+		}
+
+		private void Application_WorkbookOpen(Excel.Workbook Wb)
+		{
+			throw new NotImplementedException();
+		}
+
+		#region VSTO generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InternalStartup()
+		{
+			this.Startup += new System.EventHandler(ThisAddIn_Startup);
+			this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+		}
+
+		protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+		{
+			return new MyXMLRibbon();
+		}
+
+		#endregion
+
+		// My public methods
+
+		public static string GetResourceText(string resourceName)
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			string[] resourceNames = asm.GetManifestResourceNames();
+			for (int i = 0; i < resourceNames.Length; ++i)
+			{
+				if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+				{
+					using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+					{
+						if (resourceReader != null)
+						{
+							return resourceReader.ReadToEnd();
+						}
+					}
+				}
+			}
+			return null;
+		}
+
+		// My private methods
+
+		public string CheckVBACodeVersion(/*Excel._Workbook pWorkbook*/)
+		{
+			_latestVBACodeVersion = GetLatestVBACodeVersion();
+
+			int installedVBACodeVersion = GetInstalledVBACodeVersion(_latestVBACodeVersion, _activeWorkbook);
+
+			if (installedVBACodeVersion < _latestVBACodeVersion)
+			{
+				UpdateOrInstallVBACode(_activeWorkbook);
+			}
+
+			return "checked"; // Return some information here like "VBA macro is up to date", "VBA macro version was updated to version ..."
+		}
+
+		private int GetInstalledVBACodeVersion(int pCurrentVersion, Excel._Workbook pWorkbook)
+		{
+			// Legacy method name: VBACodeVersion
+
+			// Returns current installed VBA code version in the user's spreadsheet.
+			// Removes old version if found
+
+			int vbaCodeVersion = 0;
+
+			var project = pWorkbook.VBProject;
+
+			var projectName = project.Name;
+
+			foreach (var component in project.VBComponents)
+			{
+				VBA.VBComponent vbComponent = (VBA.VBComponent)component;
+				if (vbComponent != null)
+				{
+					string componentName = vbComponent.Name;
+					var componentCode = vbComponent.CodeModule;
+					int componentCodeLines = componentCode.CountOfLines;
+					int line = 1;
+
+					string oneLine = "";
+
+					while (line < componentCodeLines)
+					{
+
+						// Looking for the version number in the format YYMMDDHHmm
+						// This must be in the VBA code in Excel to detect if needs updated with the code in the ExcelVBA.txt that is included in this project.
+						oneLine = componentCode.Lines[line, 1];
+
+						// Only look for version if not already found
+						if (vbaCodeVersion == 0)
+						{
+							int versionSearchResult = SearchVBACodeVersionInLine(oneLine);
+							if (versionSearchResult > 0)
+							{
+								vbaCodeVersion = versionSearchResult;
+							}
+						}
+
+						line++;
+					}
+				}
+			}
+
+			return vbaCodeVersion;
+		}
+
+		private int GetLatestVBACodeVersion()
+		{
+			int vbaCodeVersion = 0;
+
+			try
+			{
+				StreamReader latestVBACodeStreamReader = GetLatestVBACodeStreamReader();
+
+				string oneLine;
+				while ((oneLine = latestVBACodeStreamReader.ReadLine()) != null)
+				{
+					int versionSearchResult = SearchVBACodeVersionInLine(oneLine);
+					if (versionSearchResult > 0)
+					{
+						vbaCodeVersion = versionSearchResult;
+						break;
+					}
+				}
+				latestVBACodeStreamReader.Close();
+			}
+			catch (Exception ex)
+			{
+				string errorMessage = ex.Message;
+
+				System.Windows.Forms.MessageBox.Show($@"Cannot get latest VBA code version.
+Error: {errorMessage}");
+			}
+
+			return vbaCodeVersion;
+		}
+
+		private static StreamReader GetLatestVBACodeStreamReader()
+		{
+			_latestVBACode = GetResourceText($"VSTOExcelAddInDemo1.{_vbaCodeFile}");
+			byte[] vbaCodeByteArray = Encoding.ASCII.GetBytes(_latestVBACode);
+			MemoryStream vbaCodeMemoryStream = new MemoryStream(vbaCodeByteArray);
+			StreamReader streamReader = new StreamReader(vbaCodeMemoryStream);
+
+			return streamReader;
+		}
+
+		private void UpdateOrInstallVBACode(Excel._Workbook pWorkbook)
+		{
+			// Need to add code to remove the old VAB code if updating an older version
+
+			try
+			{
+				var newStandardModule = pWorkbook.VBProject.VBComponents.Add(Microsoft.Vbe.Interop.vbext_ComponentType.vbext_ct_StdModule);
+
+				var newCodeModule = newStandardModule.CodeModule;
+				int lineNum = newCodeModule.CountOfLines + 1;
+
+				if (string.IsNullOrEmpty(_latestVBACode))
+				{
+					// The latestVBACode should be already assigned since it was read to find the installed version
+					// so normally should not get here
+					_latestVBACode = GetResourceText($"VSTOExcelAddInDemo1.{_vbaCodeFile}");
+				}
+				//+
+				newCodeModule.InsertLines(lineNum, _latestVBACode);
+			}
+			catch (Exception ex)
+			{
+				string errorMessage = ex.Message;
+
+				System.Windows.Forms.MessageBox.Show($@"Cannot install VBA code.
+Error: {errorMessage}");
+			}
+		}
+
+		private int SearchVBACodeVersionInLine(string pLine)
+		{
+			int vbaVersion = 0;
+
+			if (pLine.ToUpper().Replace(" ", "").IndexOf(_vbaCodeKey.ToUpper()) >= 0)
+			{
+				string vbaVersionString = pLine.Substring(pLine.IndexOf('"') + 1, 10);
+				Int32.TryParse(vbaVersionString, out vbaVersion);
+			}
+
+			return vbaVersion;
+		}
+	}
+}
